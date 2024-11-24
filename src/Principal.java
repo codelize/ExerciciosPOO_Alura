36779a3ec6a2e7d94df18f8eca8a47f7bbe5ba99
codelize <lflizier@gmail.com>
2024-11-24 19:52:28 -0300

    Encapsulamento do código

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,23 @@
+import br.com.alura.exerciciospooalura.modelos.Filme;
+
 public class Principal {
-
     public static void main(String[] args) {
-        Filme favorito = new Filme();
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
 
-        favorito.nome = "The Matrix";
-        favorito.anoDeLancamento = 1999;
-        favorito.duracaoEmMinutos = 135;
-        favorito.incluidoNoPlano = true;
 
-        favorito.exibeFichaTecnica();
-        favorito.avalia(9);
-        favorito.avalia(8);
-        favorito.avalia(9);
 
-        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        // meuFilme.somaDasAvaliacoes = 10;
+        // meuFilme.totalDeAvaliacoes = 1;
+        System.out.println(meuFilme.pegaMedia());
     }
-
 }
+
