be31978bc6e1cec2dd464acd5f62dc81fe20784e
codelize <lflizier@gmail.com>
2024-11-24 20:03:05 -0300

    Criação da classe Titulo e herança dela, para classe Filme

--- src/br/com/alura/exerciciospooalura/modelos/Filme.java
+++ src/br/com/alura/exerciciospooalura/modelos/Filme.java
@@ -1,40 +1,14 @@
 package br.com.alura.exerciciospooalura.modelos;
 
-public class Filme {
-    public String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
 
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento do filme: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
